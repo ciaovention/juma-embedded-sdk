b6d77dfc22232df641ccae781131526540e8f698
codefarmer3650 <codefarmer3650@qq.com>
2015-04-23 18:19:59 +0800

    add the blink document tools download address and two example (include Template)

---
+++ examples/Template/user.c
@@ -0,0 +1,17 @@
+#include "juma_sdk_api.h"
+
+
+
+void on_load()
+{
+	set_device_name("Template");	
+}
+
+
+void on_ready()
+{
+
+}
+
+
+
---
+++ examples/blink/user.c
@@ -0,0 +1,33 @@
+#include "juma_sdk_api.h"
+
+#define LED_1 18
+
+
+void led_on_task(void* args);
+void led_off_task(void* args);
+
+void on_load()
+{
+	set_device_name("Template");	
+}
+
+void on_ready()
+{
+	set_gpio_pin_mode(LED_1, GPIO_OUTPUT);
+  run_after_delay(led_on_task, NULL, 100);
+}
+
+void led_on_task(void* args)
+{
+  // Pull Up
+	set_gpio_pin_state(LED_1, 1);
+  run_after_delay(led_off_task, NULL, 250);
+}
+
+void led_off_task(void* args)
+{
+	// Pull Down
+	set_gpio_pin_state(LED_1, 0);
+	run_after_delay(led_on_task, NULL, 750);
+}
+
